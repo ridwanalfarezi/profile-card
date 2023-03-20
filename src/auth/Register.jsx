73154069dcec07760ed8709f131fc8336c77a377
ridwanalfarezi <alfareziridwan@gmail.com>
2023-03-20 20:32:30 +0700

    feat: login and register

---
+++ src/auth/Register.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <form action="" className="d-block">
+      <nav className="mb-5 d-flex justify-content-around">
+        <Link to="/login" className="nav-link">SIGN IN</Link>
+        <Link to="/register" className="nav-link">SIGN UP</Link>
+      </nav>
+      <div className="mb-4">
+        <input type="text" placeholder="USERNAME" className="input" required autoFocus/>
+      </div>
+      <div className="mb-5">
+        <input type="password" placeholder="PASSWORD" className="input" required/>
+      </div>
+      <button className="btn btn-lg button fw-bold d-block mx-auto rounded-pill">Login</button>
+    </form>
+  );
+};
+
+export default Login;
