6e09569bfe71d785c3663587faeb03fdfd47c505
ridwanalfarezi <alfareziridwan@gmail.com>
2023-03-21 15:32:01 +0700

    fix responsive

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,14 +8,14 @@ const Card = () => {
                 <Link to="/edit" className='edit position-absolute'>
                     <i class="bi bi-pencil-square fs-5"></i>
                 </Link>
-                <img src="ridwan.jfif" className='rounded-circle position-absolute'/>
+                <img src="ridwan.jfif" className='rounded-circle d-block position-absolute'/>
             </div>
             <div className="content">
                 <h2 className='content-name'>Ridwan Alfarezi</h2>
                 <h6 className='Address'>Jakarta, Indonesia</h6>
                 <h3 className='content-job'>Front-End Engineer</h3>
                 <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate enim cumque vel iure sequi delectus obcaecati illo nemo, itaque fugit.</p>
-                <div className='d-flex justify-content-around mt-5 fs-2 socials'>
+                <div className='d-flex justify-content-around my-5 fs-2 socials'>
                     <a href="" target="_blank">
                         <i class="bi bi-instagram"></i>
                     </a>
