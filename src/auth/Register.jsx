2bed21170673ee321baa58339bd9a2e503e18b91
ridwanalfarezi <alfareziridwan@gmail.com>
2023-03-23 12:10:17 +0700

    add validation for register

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useFormik } from "formik";
+import { Toaster } from "react-hot-toast";
+import { registerValidation } from "./validate";
 
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      fname: "",
+      username: "",
+      city: "",
+      country: "",
+      job: "",
+      password: "",
+      cpassword: "",
+    },
+    validate: registerValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="container">
-      <form action="" className="d-block">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <form action="" className="d-block" onSubmit={formik.handleSubmit}>
         <nav className="mb-5 d-flex justify-content-between">
           <NavLink to="/login" className="nav-link">
             SIGN IN
@@ -16,50 +38,72 @@ const Register = () => {
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-person-circle fs-3"></i>
           <input
+            {...formik.getFieldProps("fname")}
             type="text"
             placeholder="FULL NAME"
             className="input"
-            required
+            name="fname"
             autoFocus
           />
         </div>
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-person fs-3"></i>
           <input
+            {...formik.getFieldProps("username")}
             type="text"
             placeholder="USERNAME"
             className="input"
-            required
+            name="username"
           />
         </div>
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-building fs-3"></i>
-          <input type="text" placeholder="CITY" className="input" required />
+          <input
+            {...formik.getFieldProps("city")}
+            type="text"
+            placeholder="CITY"
+            className="input"
+            name="city"
+          />
         </div>
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-flag fs-3"></i>
-          <input type="text" placeholder="COUNTRY" className="input" required />
+          <input
+            {...formik.getFieldProps("country")}
+            type="text"
+            placeholder="COUNTRY"
+            className="input"
+            name="country"
+          />
         </div>
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-person-workspace fs-3"></i>
-          <input type="text" placeholder="JOB" className="input" required />
+          <input
+            {...formik.getFieldProps("job")}
+            type="text"
+            placeholder="JOB"
+            className="input"
+            name="job"
+          />
         </div>
         <div className="mb-4 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-key fs-3"></i>
           <input
+            {...formik.getFieldProps("password")}
             type="password"
             placeholder="PASSWORD"
             className="input"
-            required
+            name="password"
           />
         </div>
         <div className="mb-5 d-flex gap-2 align-items-center input-content">
           <i className="bi bi-key fs-3"></i>
           <input
+            {...formik.getFieldProps("cpassword")}
             type="password"
             placeholder="CONFIRM PASSWORD"
             className="input"
-            required
+            name="cpassword"
           />
         </div>
         <button
--- src/auth/validate.js
+++ src/auth/validate.js
@@ -3,19 +3,52 @@ import { toast } from "react-hot-toast";
 export async function loginValidation(values) {
   const errors = {};
   const specialChars = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
-  
+
   if (!values.username && !values.password) {
     errors.username = toast.error("Username and Password required");
   } else if (values.username.includes(" ") && values.password.includes(" ")) {
     errors.username = toast.error("Invalid Username and Password");
-  } else if (values.username.length <= 2) {
+  } else if (values.username.length < 3) {
     errors.username = toast.error("Username must be at least 3 characters");
-  } else if (values.password.length <= 8) {
+  } else if (values.password.length < 8) {
     errors.password = toast.error("Password must be at least 8 characters");
   } else if (!specialChars.test(values.password)) {
     errors.password = toast.error("Password must contain a special character!");
   }
 
+  return errors;
+}
+
+export async function registerValidation(values) {
+  const errors = {};
+  const specialChars = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
+
+  if (
+    !values.fname ||
+    !values.username ||
+    !values.city ||
+    !values.country ||
+    !values.job ||
+    !values.password ||
+    !values.cpassword
+  ) {
+    errors.fname = toast.error("Data Required!");
+  } else if (
+    /\s/.test(values.username) ||
+    /\s/.test(values.password)
+  ) {
+    errors.username = toast.error("Space doesn't allowed for username and password!");
+  } else if (values.fname.length < 4) {
+    errors.fname = toast.error("Full Name must be at least 3 characters!");
+  } else if (values.username.length < 3) {
+    errors.username = toast.error("Username must be at least 3 characters!");
+  } else if (values.password.length < 8) {
+    errors.password = toast.error("Password must be at least 8 characters!");
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error("Password must contain a special character!");
+  } else if (values.password !== values.cpassword) {
+    errors.cpassword = toast.error("Passwords must match");
+  }
 
   return errors;
 }
