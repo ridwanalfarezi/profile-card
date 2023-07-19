c92df027afb70b102c528446a99953bd8cd37cfa
ridwanalfarezi <alfareziridwan@gmail.com>
2023-07-19 14:11:43 +0700

    fix: card position to center

--- src/Card.jsx
+++ src/Card.jsx
@@ -64,43 +64,45 @@ const Card = () => {
           <BeatLoader color="#19a7ce" />
         </div>
       ) : (
-        <div className="card-container rounded-4">
-          <div className="card-bg position-relative">
-            <Link to="/edit" className="edit position-absolute">
-              <i className="bi bi-pencil-square fs-5"></i>
-            </Link>
-            <button onClick={logout} className="logout position-absolute">
-              <i className="bi bi-box-arrow-left fs-5"></i>
-            </button>
-            <img
-              src={avatar}
-              alt={fullname}
-              className="rounded-circle d-block position-absolute"
-            />
-          </div>
-          <div className="content">
-            <h2 className="content-name">{fullname}</h2>
-            <h6 className="Address">
-              {city}, {country}
-            </h6>
-            <h3 className="content-job">{job}</h3>
-            <p className="desc">{about}</p>
-            <div className="d-flex justify-content-around my-5 fs-2 socials">
-              {instagram ? (
-                <a href={instagram} target="_blank">
-                  <i className="bi bi-instagram"></i>
-                </a>
-              ) : null}
-              {facebook ? (
-                <a href={facebook} target="_blank">
-                  <i className="bi bi-facebook"></i>
-                </a>
-              ) : null}
-              {twitter ? (
-                <a href={twitter} target="_blank">
-                  <i className="bi bi-twitter"></i>
-                </a>
-              ) : null}
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <div className="card-container rounded-4">
+            <div className="card-bg position-relative">
+              <Link to="/edit" className="edit position-absolute">
+                <i className="bi bi-pencil-square fs-5"></i>
+              </Link>
+              <button onClick={logout} className="logout position-absolute">
+                <i className="bi bi-box-arrow-left fs-5"></i>
+              </button>
+              <img
+                src={avatar}
+                alt={fullname}
+                className="rounded-circle d-block position-absolute"
+              />
+            </div>
+            <div className="content">
+              <h2 className="content-name">{fullname}</h2>
+              <h6 className="Address">
+                {city}, {country}
+              </h6>
+              <h3 className="content-job">{job}</h3>
+              <p className="desc">{about}</p>
+              <div className="d-flex justify-content-around my-5 fs-2 socials">
+                {instagram ? (
+                  <a href={instagram} target="_blank">
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                ) : null}
+                {facebook ? (
+                  <a href={facebook} target="_blank">
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                ) : null}
+                {twitter ? (
+                  <a href={twitter} target="_blank">
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
